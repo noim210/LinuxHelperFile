8324ebf973c82e7670c7d984307bbcd0f35eb1a7
Mazhar <noim210@gmail.com>
2020-02-03 15:48:06 -0800

    change default monitor resolution value

--- helpfulscript/myresolution_change.sh
+++ helpfulscript/myresolution_change.sh
@@ -9,7 +9,7 @@ elif [ $# -eq 1 ]; then
     if [ "$1" == "laptop" ] || [ "$1" == "lap" ]  || [ "$1" == "l"  ]; then
         xrandr -s 5
     elif [ "$1" == "monitor" ] || [ "$1" == "mon" ] ||  [ "$1" == "m" ] ; then
-        xrandr -s 0
+        xrandr -s 1
     else    
         xrandr -s $1
     fi
